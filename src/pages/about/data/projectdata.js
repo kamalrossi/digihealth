46bb7d5f3d45b81cc66fb2f7d0fe38c0f43777a7
kamalrossi <rossikamal@gmail.com>
2022-06-28 06:52:15 +0600

    first commit

---
+++ src/pages/about/data/projectdata.js
@@ -0,0 +1,50 @@
+const projectowner = "Rossi Kamal(rossikamal@gmail.com)";
+const projectname = "digiHealth";
+
+const projectdesc = {
+  title: "Title",
+  desc: "digiHealth is responsive web-app for  online health facilitator platform having services such as doctor appointment. Major modules include service, department, testimonial, doctors and contact."
+};
+const projecttag = [
+  {
+    tag1: "ReactJS",
+    tag3: "JSX",
+    tag4: "MedTech"
+  }
+];
+
+const projecttime = [
+  {
+    title: "Design",
+    from: "April 22",
+    to: "June 22"
+  },
+  {
+    title: "Implementation",
+    from: "May 22",
+    to: "Jun 22"
+  }
+];
+
+const projectrole = [
+  {
+    title: "UI & UX Design",
+    desc:
+      "Requirement analysis and determination of  the major feature, service , module of online medical platform",
+  },
+  {
+    title: "Responsive Web development",
+    desc:
+      "Develop ReactJS based Responsive landing page of medical platform with homepage, service, feature, doctor, appointment modules"
+  },
+  
+];
+
+export {
+  projectowner,
+  projectname,
+  projectdesc,
+  projectrole,
+  projecttime,
+  projecttag
+};
